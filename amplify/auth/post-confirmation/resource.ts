5cecdb87fe5abb9ed8a8511f7805cfa123d5f3e0
Joe <joseph_a_f@hotmail.com>
2024-09-01 11:53:31 -0700

    corrections

--- amplify/auth/post-confirmation/resource.ts
+++ amplify/auth/post-confirmation/resource.ts
@@ -1,11 +1,6 @@
-import { defineAuth } from '@aws-amplify/backend';
-import { postConfirmation } from './post-confirmation/resource';
+//test
+import { defineFunction } from '@aws-amplify/backend';
 
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
-  triggers: {
-    postConfirmation
-  }
+export const postConfirmation = defineFunction({
+  name: 'post-confirmation',
 });
--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,14 +1,14 @@
-import { defineAuth } from '@aws-amplify/backend';
+// import { defineAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
-});
+// /**
+//  * Define and configure your auth resource
+//  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+//  */
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+//   },
+// });
 
 
 // import { defineAuth } from '@aws-amplify/backend';
@@ -22,3 +22,16 @@ export const auth = defineAuth({
 //     postConfirmation
 //   }
 // });
+
+
+import { defineAuth } from '@aws-amplify/backend';
+import { postConfirmation } from './post-confirmation/resource';
+
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  triggers: {
+    postConfirmation
+  }
+});
